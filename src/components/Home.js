641d4f804cac969e5f8cd84be97e751a1b00f3c1
harshilchandratre <harshilchandratre2002@gmail.com>
2022-12-13 23:31:23 +0530

    reverted version, minor changes v1.2

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 import logo from './logo.png';
 import image1 from './image1.jpg';
 // import background from './background.jpeg';
-import './Home.css';
-// import '../App.css';
+import './Home.css'
+// import '../App.css'
 
 const Home = () => {
     return (
@@ -42,14 +42,20 @@ const Home = () => {
             <p>Welcome to Homepage</p>
 
             <div className="container">
-                <h1 className="Heading">
+                {/* <h1 className="Heading">
                     What is PaveRM ?
                 </h1>
                 <h3 className="subHeading">
                     A computer science portal
                 </h3>
-                <br />
+                <br /> */}
                 <table cellSpacing="60px" cellPadding="60px">
+                    <thead>
+                        <tr>
+                            <td><label className="Heading">What is PaveRM?</label><br/><label className="subHeading">A Road Reporting Portal</label></td>
+                            <td></td>
+                        </tr>
+                    </thead>
                     <tr>
                         <td><img className="subImg" src={image1} alt="img" /></td>
                         <td><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo rerum, laudantium sunt culpa illum, aliquid dicta perspiciatis odio cumque laborum, similique cum deleniti architecto blanditiis impedit autem praesentium nostrum? Optio eveniet minima labore voluptatum deserunt quo nulla similique quaerat, eaque fuga qui temporibus itaque architecto sequi tempora. Dolorem odit nostrum qui minima eaque excepturi, commodi doloribus rem asperiores architecto totam animi sequi esse, voluptatibus vel sed, ratione accusamus doloremque perferendis? A molestias placeat error minus laboriosam voluptatum in ipsa. Amet corporis, a ullam impedit pariatur id temporibus omnis odit incidunt. Tempora, cumque vel. Veritatis, placeat provident? Totam quasi repudiandae debitis?</p></td>
@@ -69,8 +75,7 @@ const Home = () => {
                         <td><img className="subImg" src={image1} alt="img" /></td>
                     </tr>
                     <tr>
-                        <td></td>
-                        <td></td>
+                        
                     </tr>
                 </table>
             </div>
@@ -82,7 +87,18 @@ const Home = () => {
                         href="#"> harshilchandratre.com</a>
                 </div>
             </footer> */}
-            </div>
+
+
+
+
+
+
+
+
+
+
+
+        </div>
         // </div>
 
 
